47fa6d9384b6f2ca8d6b762986b4a9c0a20d2db0
shivamXCodes <shivam.codes123@gmail.com>
2025-09-27 02:07:28 +0530

    Time: 1 ms (71.42%) | Memory: 117.9 MB (84.57%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -15,8 +15,8 @@ public:
             return head;
         }
         ListNode* slow = head;
-        ListNode* fast = head;
-        while(fast->next!=NULL && fast->next->next!=NULL ){
+        ListNode* fast = head->next;
+        while(fast!=NULL && fast->next!=NULL){
           slow = slow->next;
           fast = fast->next->next;
         }
